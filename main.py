713e4266c00bbc9b9e9805b5b5987fe7edfeb410
Valfien <flavderoche@orange.fr>
2025-05-20 14:42:22 +0200

    Renaissance

--- main.py
+++ main.py
@@ -4,4 +4,219 @@ aliens.main()
 
 
 
-#test
+import pygame
+from variables import *
+# Initialisation de pygame et de la fenêtre
+pygame.init()
+
+def gestion_touche(perso,liste_mur,liste_spike,s,distance):
+    keys = pygame.key.get_pressed()
+    s.arrivee = hauteur//2
+    # Gestion du saut
+    if (keys[pygame.K_SPACE] or keys[pygame.K_UP])and s.saut_en_cours == 0:
+        s.saut_en_cours = 1
+        s.position_saut = 0
+        s.phase_saut = 1
+        s.arrivee = hauteur // 2
+        s.vitesse = 1
+        s.depart = perso.y
+
+    if s.saut_en_cours == 1:
+        perso.y = 0.05*s.vitesse*s.vitesse - 5*s.vitesse + s.depart
+        
+        s.vitesse += 1
+        if s.phase_saut == 1:
+            s.position_saut += 1
+            if s.position_saut > 50:
+                s.phase_saut = 0
+        else :
+            s.position_saut -= 1
+            if perso.y + perso_hauteur > s.arrivee:
+                s.saut_en_cours = 0
+                s.phase_saut = 1
+                perso.y  = s.arrivee - perso_hauteur
+        
+    # Déplacement horizontal
+    if keys[pygame.K_LEFT]:
+        perso.x -= perso.vitesse
+        perso.sens = 0  
+    if keys[pygame.K_RIGHT]:
+        if(perso.x >= largeur // 2):
+            distance+= 1
+        perso.sens = 1  
+        if perso.x < largeur // 2:
+            perso.x += perso.vitesse
+        else:
+            for objet_mur in liste_mur:
+                objet_mur.x -= perso.vitesse  # Déplacer le mur vers la gauche
+            for spikes in liste_spike:
+                spikes.x -= perso.vitesse
+
+    if keys[pygame.K_c]:
+        if len(fireballs) < 1:
+            fireballs.append(fireball(perso.x+perso_largeur, perso.y-perso_hauteur/2, 50, 50))
+
+    # Collision avec le mur
+    for objet_mur in liste_mur:
+        if perso.y+perso_hauteur <= objet_mur.y: # Si le joueur est au-dessus du mur
+            s.arrivee = objet_mur.y 
+            s.sur_le_mur = False
+            print("zone1")      
+        else:                                   # Si le joueur touche le mur
+            if perso.x+perso_largeur > objet_mur.x and perso.x < objet_mur.x + objet_mur.largeur and perso.y+perso_hauteur <= objet_mur.y+objet_mur.hauteur: # Si le joueur dans l'aire du mur
+                s.sur_le_mur = True
+                s.saut_en_cours == 0
+                perso.y = objet_mur.y- perso_hauteur
+                
+            else:
+                s.sur_le_mur = False
+                if perso.y+perso_hauteur > objet_mur.y and perso.y<objet_mur.y+objet_mur.hauteur:
+                    
+                    if perso.x+perso_largeur >= objet_mur.x and perso.x < objet_mur.x + objet_mur.largeur and perso.sens == 1:
+                        perso.x = objet_mur.x - perso_largeur - perso.vitesse
+                    elif perso.x+perso_largeur > objet_mur.x and perso.x <= objet_mur.x + objet_mur.largeur and perso.sens == 0:
+                        perso.x = objet_mur.x + objet_mur.largeur + perso.vitesse
+        
+        
+        if objet_mur.x < -objet_mur.largeur:  # Si le mur sort de l'écran, le remettre à droite
+            objet_mur.x = largeur-objet_mur.largeur+400
+    for spikes in liste_spike:
+        if spikes.x < -spikes.largeur:  # Si le mur sort de l'écran, le remettre à droite
+                spikes.x = largeur-spikes.largeur+400
+
+    if s.sur_le_mur==False and s.saut_en_cours == 0:
+        perso.y = min( hauteur//2-perso_hauteur, perso.y + 1 )  # Si le joueur ne touche pas le mur et n'est pas en saut, il tombe
+
+    # Empêcher le joueur de sortir de l'écran
+    perso.x = max(0, min(largeur - perso_largeur, perso.x))
+    perso.y = max(0, min(hauteur - perso_hauteur, perso.y))
+
+    #objet_mur.x -= game_speed  # Déplacer le mur vers la gauche
+
+    return perso, liste_mur,liste_spike, distance
+
+def affichage_boutton(screen, text, x, y, width, height, color, text_color): #exemple trouver sur internet à peut être améliorer
+    pygame.draw.rect(screen, color, (x, y, width, height))  # Dessiner le rectangle du bouton
+    font = pygame.font.Font(None, 36)  # Police par défaut, taille 36
+    text_surface = font.render(text, True, text_color)  # Rendre le texte
+    text_rect = text_surface.get_rect(center=(x + width // 2, y + height // 2))  # Centrer le texte
+    screen.blit(text_surface, text_rect)  # Afficher le texte
+
+# Boucle principale
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  # Fermer la fenêtre
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN :  # Si un clic est détecté
+            mouse_x, mouse_y = event.pos  # Position de la souris
+            if bouton_x <= mouse_x <= bouton_x + bouton_width and bouton_y <= mouse_y <= bouton_y + bouton_height:
+                # Réinitialiser les variables du jeu
+                perso.x = largeur // 2
+                perso.y = hauteur // 2- perso_hauteur
+                ennemie1.x = largeur
+                ennemie1.y = hauteur // 2 - ennemie1.hauteur
+                liste_mur =[Mur(0,  0,  50, 40, "image/mur_de10.png"),
+                            Mur(210, 0, 200, 20, "image/mur_de10.png"),
+                            Mur(220, 30,  10, 30, "image/mur_de10.png")]  # Liste des obstacles
+                liste_spike= [spike(700, 0, 100, 30, "perso1.png"),
+                              spike(300, 0, 100, 30, "perso1.png")]
+                play_again = True
+
+
+    if play_again:  # Si le jeu est en cours
+        # Afficher l'image de fond
+        screen.blit(background_image, (0, 0))
+
+        # Mettre à jour la position du joueur
+        perso,liste_mur,liste_spike,distance = gestion_touche(perso,liste_mur,liste_spike,s,distance)
+       
+
+
+        #mettre a jour position ennemi a modifier il fait pas des aller retour
+        for ennemie1 in ennemie:
+            if ennemie1.x < 0:
+                ennemie1.x = largeur
+            elif ennemie1.x > largeur:
+                ennemie1.x = 0
+            else:
+                ennemie1.x -= game_speed
+
+        #mettre a jour position fireball
+        for firebal in fireballs:
+            firebal.x += game_speed*5
+            if firebal.x > largeur:
+                fireballs.remove(firebal)
+        # Afficher les fireballs
+        for firebal in fireballs:
+            screen.blit(firebal.image, (firebal.x, firebal.y))
+            for ennemie1 in ennemie:
+                if (firebal.x + firebal.largeur > ennemie1.x and firebal.x < ennemie1.x + ennemie1.largeur and firebal.y + firebal.hauteur > ennemie1.y and firebal.y < ennemie1.y + ennemie1.hauteur):
+                    fireballs.remove(firebal)
+                    ennemie.remove(ennemie1)
+        for objet_mur in liste_mur:
+            for firebal in fireballs:
+                if (firebal.x + firebal.largeur > objet_mur.x and firebal.x < objet_mur.x + objet_mur.largeur and firebal.y + firebal.hauteur > objet_mur.y and firebal.y < objet_mur.y + objet_mur.hauteur):
+                    fireballs.remove(firebal)
+                
+        
+
+        # Vérifier la collision entre le perso et les spikes et ennemis
+        for ennemie1 in ennemie:
+            if (perso.x + perso_largeur > ennemie1.x and perso.x < ennemie1.x + ennemie1.largeur and perso.y + perso_hauteur > ennemie1.y and perso.y < ennemie1.y + ennemie1.hauteur):
+                play_again = False
+        
+        
+        # Dessiner le mur
+        for objet_mur in liste_mur:
+            screen.blit(objet_mur.image, (objet_mur.x, objet_mur.y))
+        for spikes in liste_spike:
+            screen.blit(spikes.image, (spikes.x, spikes.y))  # Afficher le spike
+            if (perso.x + perso_largeur > spikes.x and perso.x < spikes.x + spikes.largeur and perso.y + perso_hauteur > spikes.y and perso.y < spikes.y + spikes.hauteur):
+                #play_again = False
+                print("collision avec le spike")
+            
+
+
+       #animation courir / sauter
+        keys = pygame.key.get_pressed()
+        if s.saut_en_cours:                                   # saut
+             perso_image_actuelle = perso_image7
+        elif keys[pygame.K_m]:                                # course au sol
+            perso_image_actuelle = (
+                perso_image1 if (pygame.time.get_ticks() // 100) % 2 == 0 else perso_image2
+            )
+        else:                                                 # immobile
+            perso_image_actuelle = perso_image1
+
+        # Afficher le perso
+        screen.blit(perso_image_actuelle, (perso.x, perso.y))
+        font = pygame.font.Font(None, 20)  # Taille du texte
+        texte = font.render(f"Score:{distance}", True, (0, 0, 0))  # Blanc
+        screen.blit(texte, (940, 10))  # Position (x=20, y=10)
+        for ennemie1 in ennemie:
+            if (ennemie1.temps_anim==0):
+                screen.blit(ennemie1.image1, (ennemie1.x, ennemie1.y))
+                ennemie1.temps_anim=ennemie1.temps_anim+1
+            elif (ennemie1.temps_anim==1):
+                screen.blit(ennemie1.image2, (ennemie1.x, ennemie1.y))
+                ennemie1.temps_anim=ennemie1.temps_anim+1
+            elif (ennemie1.temps_anim==2):
+                screen.blit(ennemie1.image3, (ennemie1.x, ennemie1.y))
+                ennemie1.temps_anim=0
+
+    else:  # Si le jeu est terminé
+        # Afficher l'écran de Game Over
+
+        screen.blit(gameover_image, (0, 0))
+        texte = font.render(f"Score:{distance}", True, (255,255,255))  # Blanc
+        screen.blit(texte, (largeur//2-25, 50))  # Position (x=20, y=10)
+
+        # Afficher le bouton "Rejouer"
+        affichage_boutton(screen, "Rejouer", bouton_x, bouton_y, bouton_width, bouton_height, (255, 0, 0), (255, 255, 255))
+
+    # Mettre à jour l'affichage
+    pygame.display.flip()
+
+# Quitter pygame
+pygame.quit()
---
+++ variables.py
@@ -0,0 +1,134 @@
+import pygame
+
+# Dimensions de la fenêtre
+largeur, hauteur = 1000, 600
+play_again = True
+game_speed = 1  # Vitesse de déplacement du mur
+distance = 0
+
+#Bouton rejouer
+bouton_width, bouton_height = 200, 50
+bouton_x = (largeur - bouton_width) // 2  # Centré horizontalement
+bouton_y = ((hauteur - bouton_height) // 10)*9  # Centré verticalement
+
+# Perso
+perso_image = pygame.image.load("Capture d'écran 2024-09-27 201400.png") 
+perso_largeur, perso_hauteur = 50, 50  # Taille du perso
+perso_image = pygame.transform.scale(perso_image, (perso_largeur, perso_hauteur))  # Redimensionner l'image du perso
+class Joueur:
+    def __init__(self, x, y, vitesse):
+        self.x = x 
+        self.y = y
+        self.vitesse = vitesse #vitesse de déplacement du perso
+        self.image = pygame.transform.scale(perso_image, (50, 50))
+        self.rect = self.image.get_rect(topleft=(x, y))
+        self.sens = 1
+
+perso = Joueur(largeur//2, hauteur//2-perso_hauteur, 5)  # Initialisation du persos
+perso = Joueur(largeur//2, hauteur//2, 5)  # Initialisation du perso
+perso_image1 = pygame.image.load("perso1.png")
+perso_image2 = pygame.image.load("perso2.png") 
+perso_image7 = pygame.image.load("perso7.png") 
+perso_image8 = pygame.image.load("perso8.png") 
+
+perso_largeur, perso_hauteur = 50, 50  # Taille du perso
+perso_image1 = pygame.transform.scale(perso_image1, (perso_largeur, perso_hauteur))  # Redimensionner l'image1 du perso
+perso_image2 = pygame.transform.scale(perso_image2, (perso_largeur, perso_hauteur))  # Redimensionner l'image2 du perso
+perso_image7 = pygame.transform.scale(perso_image7, (perso_largeur, perso_hauteur))  # Redimensionner l'image2 du perso
+perso_image8= pygame.transform.scale(perso_image8, (perso_largeur, perso_hauteur))  # Redimensionner l'image2 du perso
+perso_image_actuelle = perso_image1
+
+
+
+
+class Mur:
+    def __init__(self, x, y, murlargeur, murhauteur, image):
+        self.image = pygame.image.load(image)
+        self.x = murlargeur+x
+        self.y = hauteur//2-y-murhauteur
+        self.largeur = murlargeur
+        self.hauteur = murhauteur
+        self.image = pygame.transform.scale(self.image, (self.largeur, self.hauteur))
+        
+liste_mur = [Mur(0,  0,  50, 40, "image/mur_de10.png"),
+             Mur(210, 0, 200, 20, "image/mur_de10.png"),
+             Mur(220, 100,  150, 30, "image/mur_de10.png")]  # Liste des obstacles
+
+#creation de spikes
+class spike:
+    def __init__(self, x, y, spikelargeur, spikehauteur, image):
+        self.image = pygame.image.load(image)
+        self.x = spikehauteur+x
+        self.y = hauteur//2-y-spikehauteur
+        self.largeur = spikelargeur
+        self.hauteur = spikehauteur
+        self.image = pygame.transform.scale(self.image, (self.largeur, self.hauteur))
+liste_spike= [spike(700, 0, 100, 30, "perso1.png"),
+               spike(300, 0, 100, 30, "perso1.png")]
+
+class fireball:
+    def __init__(self, x, y, largeur, hauteur):
+        self.image = pygame.image.load("image/fireball.png")
+        self.x = x
+        self.y = y
+        self.largeur = largeur
+        self.hauteur = hauteur
+        self.image = pygame.transform.scale(self.image, (self.largeur, self.hauteur))
+
+fireballs=[]
+
+class Saut:
+    def __init__(self,saut_en_cours,position_saut,phase_saut):
+        self.saut_en_cours=saut_en_cours
+        self.position_saut=position_saut
+        self.phase_saut=phase_saut
+        self.sur_le_mur=0
+        self.arrivee=hauteur//2
+        self.vitesse=1
+        self.depart=hauteur//2
+s=Saut(0,0,1)
+
+
+#Game over
+gameover_image = pygame.image.load("game_over.png")
+gameover_image = pygame.transform.scale(gameover_image, (largeur, hauteur))
+
+# Création de la fenêtre
+screen = pygame.display.set_mode((largeur, hauteur))
+pygame.display.set_caption("Jeu de OUUUUUF") # Titre de la fenêtre
+# Charger l'image de fond
+background_image = pygame.image.load("Fond.png") 
+background_image = pygame.transform.scale(background_image, (largeur, hauteur))  
+
+
+perso = Joueur(largeur//2, hauteur//2-perso_hauteur, 5)  # Initialisation du persos
+perso = Joueur(largeur//2, hauteur//2, 5)  # Initialisation du perso
+perso_image1 = pygame.image.load("perso1.png")
+perso_image2 = pygame.image.load("perso2.png") 
+perso_image7 = pygame.image.load("perso7.png") 
+perso_image8 = pygame.image.load("perso8.png") 
+
+perso_largeur, perso_hauteur = 50, 50  # Taille du perso
+perso_image1 = pygame.transform.scale(perso_image1, (perso_largeur, perso_hauteur))  # Redimensionner l'image1 du perso
+perso_image2 = pygame.transform.scale(perso_image2, (perso_largeur, perso_hauteur))  # Redimensionner l'image2 du perso
+perso_image7 = pygame.transform.scale(perso_image7, (perso_largeur, perso_hauteur))  # Redimensionner l'image2 du perso
+perso_image8= pygame.transform.scale(perso_image8, (perso_largeur, perso_hauteur))  # Redimensionner l'image2 du perso
+perso_image_actuelle = perso_image1
+
+class ennemi:
+    def __init__(self, x, y, largeur, hauteur):
+        self.image1 = pygame.image.load("image/ennemi1.png")
+        self.image2 = pygame.image.load("image/ennemi2.png")
+        self.image3 = pygame.image.load("image/ennemi3.png")
+        self.x = x
+        self.y = y
+        self.largeur = largeur
+        self.hauteur = hauteur
+        self.image1 = pygame.transform.scale(self.image1, (self.largeur, self.hauteur))
+        self.image2 = pygame.transform.scale(self.image2, (self.largeur, self.hauteur))
+        self.image3 = pygame.transform.scale(self.image3, (self.largeur, self.hauteur))
+        self.temps_anim=0
+    
+ennemie1 = ennemi(largeur, hauteur//2-50,27,50)
+
+ennemie=[ennemie1]
